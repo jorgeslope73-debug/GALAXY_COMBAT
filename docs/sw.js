74af5d5a5abd36b0fd1c997485640f8f11353b15
jorgeslope73-debug <jorgeslope73@gmail.com>
2026-09-24 13:59:46 +0200

    V18.24: alinear controles de la sala

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'V18.23';
+const VERSION = 'V18.24';
 const SHELL_CACHE = `galaxy-combat-shell-${VERSION}`;
 const ASSET_CACHE = `galaxy-combat-assets-${VERSION}`;
 const ACTIVE_CACHES = new Set([SHELL_CACHE, ASSET_CACHE]);
