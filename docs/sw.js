12628990cb62ca8a31011a3cd562f13736a047ac
jorgeslope73-debug <jorgeslope73@gmail.com>
2026-09-25 16:11:56 +0200

    V18.57: sumar una baja al recoger la mira

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'V18.56';
+const VERSION = 'V18.57';
 const SHELL_CACHE = `galaxy-combat-shell-${VERSION}`;
 const ASSET_CACHE = `galaxy-combat-assets-${VERSION}`;
 const ACTIVE_CACHES = new Set([SHELL_CACHE, ASSET_CACHE]);
