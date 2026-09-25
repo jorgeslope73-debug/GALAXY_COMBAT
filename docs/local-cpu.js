db84ba609f6f812044793d6157cd04f2b26862e6
jorgeslope73-debug <jorgeslope73@gmail.com>
2026-09-26 00:37:11 +0200

    V18.69: completar mira con bala y actualizar cache

--- docs/local-cpu.js
+++ docs/local-cpu.js
@@ -968,6 +968,7 @@
             }
             else if(pk.type==='cadence')p.cadence=Math.max(1,p.cadence-10);
             else if(pk.type==='mira'){
+              if(p.bullets<=0)p.bullets=1;
               p.guided=true;p.guidedTarget=this.guidedTargetFor(p);
             }
             else if(pk.type==='speed')p.speed=Math.min(2,p.speed+.5);
