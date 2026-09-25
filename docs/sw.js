16ff2eed6bbf7d05f4c87841b3ff31308fe815b5
jorgeslope73-debug <jorgeslope73@gmail.com>
2026-09-26 01:42:29 +0200

    V18.77: actualizar version y cache

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'V18.76';
+const VERSION = 'V18.77';
 const SHELL_CACHE = `galaxy-combat-shell-${VERSION}`;
 const ASSET_CACHE = `galaxy-combat-assets-${VERSION}`;
 const ACTIVE_CACHES = new Set([SHELL_CACHE, ASSET_CACHE]);
