e69df9b3c9bbd57babc07dadeccced9961df2566
jorgeslope73-debug <jorgeslope73@gmail.com>
2026-09-26 00:49:49 +0200

    V18.72: completar traducciones de compartir juego

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'V18.71';
+const VERSION = 'V18.72';
 const SHELL_CACHE = `galaxy-combat-shell-${VERSION}`;
 const ASSET_CACHE = `galaxy-combat-assets-${VERSION}`;
 const ACTIVE_CACHES = new Set([SHELL_CACHE, ASSET_CACHE]);
