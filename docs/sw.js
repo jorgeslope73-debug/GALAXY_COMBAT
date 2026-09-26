a4310b8553ba303df5eb64c7f79ddce9ba6620fe
jorgeslope73-debug <jorgeslope73@gmail.com>
2026-09-26 16:49:13 +0200

    V18.92: actualizar service worker

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'V18.91';
+const VERSION = 'V18.92';
 const SHELL_CACHE = `galaxy-combat-shell-${VERSION}`;
 const ASSET_CACHE = `galaxy-combat-assets-${VERSION}`;
 const ACTIVE_CACHES = new Set([SHELL_CACHE, ASSET_CACHE]);
